53251e15fab6211259a13530fda40717bcfbb397
Patricia Kore <patricia.kore@yahoo.com>
2015-04-24 11:34:36 -0400

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create]
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,23 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  # cities = City.create(
+  #   [
+  #     { name: 'Chicago' }, 
+  #     { name: 'Copenhagen' }
+  #   ]
+  # )
+
+  # Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+my_users = User.create(
+    [
+       {email: "patricia.kore@yahoo.com", password: "1234"},
+       {email: "a@yahoo.com", password: "12345"},
+       {email: "b@yahoo.com", password: "123456"}
+    ]
+
+
+  )
